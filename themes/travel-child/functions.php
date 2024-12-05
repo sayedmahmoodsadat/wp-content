2629769c6502270330603ca4fc5174ff68c81bf5
SAYED MAHMOOD SADAT <sayedmahmood.sadat@dcmail.ca>
2024-12-05 05:18:40 -0500

    Solving the mistake instead of ./ replaced with /

--- themes/travel-child/functions.php
+++ themes/travel-child/functions.php
@@ -12,7 +12,7 @@ function child_enqueue_scripts() {
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' );
 
     if ( is_user_logged_in() ) {
-        wp_enqueue_style( 'app-style', get_stylesheet_directory_uri() . './assets/css/app.css'  );
+        wp_enqueue_style( 'app-style', get_stylesheet_directory_uri() . '/assets/css/app.css'  );
     }    
 }
 add_action( 'wp_enqueue_scripts', 'child_enqueue_scripts' );
