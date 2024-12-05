6c66838c3f1372697daa8b2f9323a2911fe786c3
SAYED MAHMOOD SADAT <sayedmahmood.sadat@dcmail.ca>
2024-12-05 01:02:09 -0500

    Style coded being add to my child theme

---
+++ themes/travel-child/functions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Travel-child functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Travel-child 
+ * @since Travel-child  1.0
+ */
+
+function child_enqueue_scripts() {
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' );
+
+    if ( is_user_logged_in() ) {
+        wp_enqueue_style( 'app-style', get_stylesheet_directory_uri() . './assets/css/app.css'  );
+    }    
+}
+add_action( 'wp_enqueue_scripts', 'child_enqueue_scripts' );
+
+?>
